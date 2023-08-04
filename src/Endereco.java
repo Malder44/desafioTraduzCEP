7e1700921d99f1f0df4d6981fbbbbec3f3f95041
Leonorico <leonorico2epb@gmail.com>
2023-08-04 15:01:48 -0300

    Criando classe que reúne informações de endereço referente ao CEP

---
+++ src/Endereco.java
@@ -0,0 +1,30 @@
+public class Endereco {
+
+    private int cep;
+    private String rua;
+    private String complemento;
+    private String bairro;
+    private String municipio;
+    private String estado;
+
+    public Endereco(EnderecoViaCep enderecoViaCep) {
+        this.cep = Integer.valueOf(enderecoViaCep.cep().replace("-",""));
+        this.rua = enderecoViaCep.logradouro();
+        this.complemento = enderecoViaCep.complemento();
+        this.bairro = enderecoViaCep.bairro();
+        this.municipio = enderecoViaCep.localidade();
+        this.estado = enderecoViaCep.uf();
+    }
+
+    @Override
+    public String toString() {
+        return "(" +
+                "CEP = \"" + cep + "\"" +
+                ", Rua = \"" + rua + "\"" +
+                ", Complemento = \"" + complemento + "\"" +
+                ", Bairro = \"" + bairro + "\"" +
+                ", Municipio = \"" + municipio + "\"" +
+                ", Estado = \"" + estado + "\"" +
+                ')';
+    }
+}
