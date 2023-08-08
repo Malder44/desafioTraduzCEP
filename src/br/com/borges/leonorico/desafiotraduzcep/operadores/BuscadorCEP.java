ffcc9da9bf9e6034968c43911b9f42bf698b2d4c
Leonorico <leonorico2epb@gmail.com>
2023-08-07 21:43:09 -0300

    Criação e desenvolvimento da classe que consome a API 'ViaCEP', juntamente com os devidos tratamentos de exceções

--- src/br/com/borges/leonorico/desafiotraduzcep/operadores/BuscadorCEP.java
+++ src/br/com/borges/leonorico/desafiotraduzcep/operadores/BuscadorCEP.java
@@ -1,19 +1,38 @@
 package br.com.borges.leonorico.desafiotraduzcep.operadores;
 
+import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
-public class ConsultaCep {
+public class BuscadorCEP {
 
-    link = "https://viacep.com.br/ws/" + busca + "/json/";
-            System.out.println(link);
-    HttpClient client = HttpClient.newHttpClient();
-    HttpRequest request = HttpRequest.newBuilder()
-            .uri(URI.create(link))
-            .build();
-    HttpResponse<String> response = client
-            .send(request, HttpResponse.BodyHandlers.ofString());
-            System.out.println(response.body());
+    HttpClient client;
+    HttpRequest request;
+    HttpResponse<String> response;
+
+    public BuscadorCEP() {
+        client = HttpClient.newHttpClient();
+    }
+
+    public String consultaCEP(String busca) {
+        if(busca.charAt(0) == '\n')
+            throw new IllegalArgumentException("Entrada inválida");
+        for(int i = 0; i < busca.length(); i++) {
+            if(busca.charAt(i) < '0' || busca.charAt(i) > '9') {
+                throw new IllegalArgumentException("CEP inválido");
+            }
+        }
+        if(busca.length() != 8)
+            throw new IllegalArgumentException("Número de dígitos incorreto.");
+        busca = "https://viacep.com.br/ws/" + busca + "/json/";
+        request = HttpRequest.newBuilder().uri(URI.create(busca)).build();
+        try {
+            response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        } catch (InterruptedException | IOException e) {
+            throw new RuntimeException("Ocorreu um erro inesperado.");
+        }
+        return response.body();
+    }
 }
