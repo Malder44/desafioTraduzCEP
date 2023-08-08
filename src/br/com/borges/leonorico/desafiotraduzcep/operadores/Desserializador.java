4465292db13196610fd9a77b2ec4b0e75131f3d5
Leonorico <leonorico2epb@gmail.com>
2023-08-07 21:44:07 -0300

    Criação e desenvolvimento da classe que atribui os dados em JSON aos atributos da classe 'EnderecoViaCep', juntamente com os tratamentos de exceções

--- src/br/com/borges/leonorico/desafiotraduzcep/operadores/Desserializador.java
+++ src/br/com/borges/leonorico/desafiotraduzcep/operadores/Desserializador.java
@@ -1,2 +1,25 @@
-package br.com.borges.leonorico.desafiotraduzcep.operadores;public class Conversores {
+package br.com.borges.leonorico.desafiotraduzcep.operadores;
+
+import br.com.borges.leonorico.desafiotraduzcep.Endereco;
+import br.com.borges.leonorico.desafiotraduzcep.EnderecoViaCep;
+import com.google.gson.Gson;
+
+import java.util.NoSuchElementException;
+
+public class Desserializador {
+
+    Gson gson;
+    EnderecoViaCep enderecoViaCep;
+
+    public Desserializador() {
+        gson = new Gson();
+    }
+
+    public Endereco converteJsonParaEndereco(String json) {
+        enderecoViaCep = gson.fromJson(json, EnderecoViaCep.class);
+        if(enderecoViaCep.erro()) {
+            throw new NoSuchElementException("Esse CEP não existe.");
+        }
+        return new Endereco(enderecoViaCep);
+    }
 }
