d3d9f6ce15944e9be713ac9a07a85dca8a752428
Leonorico <leonorico2epb@gmail.com>
2023-08-07 21:45:59 -0300

    Reformulação do código principal da aplicação e inserção de tratamento de exceções, com retorno visual ao usuário

--- src/br/com/borges/leonorico/desafiotraduzcep/Main.java
+++ src/br/com/borges/leonorico/desafiotraduzcep/Main.java
@@ -1,55 +1,42 @@
 package br.com.borges.leonorico.desafiotraduzcep;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
+import br.com.borges.leonorico.desafiotraduzcep.operadores.BuscadorCEP;
+import br.com.borges.leonorico.desafiotraduzcep.operadores.Desserializador;
+import br.com.borges.leonorico.desafiotraduzcep.operadores.Serializador;
 
-import java.io.FileWriter;
-import java.io.IOException;
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException, InterruptedException {
+    public static void main(String[] args) {
 
         Scanner teclado = new Scanner(System.in);
-        String busca = "", link;
+        String busca = "", enderecoJson;
+        Endereco endereco;
+        BuscadorCEP buscador = new BuscadorCEP();
+        Desserializador desserializador = new Desserializador();
+        Serializador serializador = new Serializador();
         List<Endereco> enderecos = new ArrayList<>();
-        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+
         while(!busca.equalsIgnoreCase("sair")) {
             System.out.println("Informe o CEP para busca: ");
             busca = teclado.nextLine();
-
-            if(busca.equalsIgnoreCase("sair"))
-                break;
-
-            //ConsultaCep
-            link = "https://viacep.com.br/ws/" + busca + "/json/";
-            System.out.println(link);
-            HttpClient client = HttpClient.newHttpClient();
-            HttpRequest request = HttpRequest.newBuilder()
-                    .uri(URI.create(link))
-                    .build();
-            HttpResponse<String> response = client
-                    .send(request, HttpResponse.BodyHandlers.ofString());
-            System.out.println(response.body());
-            //Desserializador
-            EnderecoViaCep enderecoViaCep = gson.fromJson(response.body(), EnderecoViaCep.class);
-            System.out.println(enderecoViaCep);
-            Endereco endereco = new Endereco(enderecoViaCep);
-            System.out.println(endereco.toString());
-            //ListaEndereco
-            enderecos.add(endereco);
-            System.out.println(enderecos);
+            if(!busca.equalsIgnoreCase("sair")) {
+                try {
+                    enderecoJson = buscador.consultaCEP(busca);
+                    endereco = desserializador.converteJsonParaEndereco(enderecoJson);
+                    enderecos.add(endereco);
+                } catch(IllegalArgumentException | NoSuchElementException e) {
+                    System.out.println(e.getMessage());
+                } catch(RuntimeException e) {
+                    System.out.println(e.getMessage());
+                }
+            }
         }
-        //Serializador
-        FileWriter writer = new FileWriter("enderecos.json");
-        writer.write(gson.toJson(enderecos));
-        writer.close();
+        try {
+            serializador.converteListaEnderecosParaJson(enderecos);
+        } catch(RuntimeException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
