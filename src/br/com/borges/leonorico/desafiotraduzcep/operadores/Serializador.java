8df7c7790b3aff36a4839386aa7f30e73f7d6667
Leonorico <leonorico2epb@gmail.com>
2023-08-07 21:44:56 -0300

    Criação e desenvolvimento da classe 'Serializador', que converte os endereços em JSON e escreve-os em arquivo

--- src/br/com/borges/leonorico/desafiotraduzcep/operadores/Serializador.java
+++ src/br/com/borges/leonorico/desafiotraduzcep/operadores/Serializador.java
@@ -1,2 +1,28 @@
-package br.com.borges.leonorico.desafiotraduzcep.operadores;public class Serializador {
+package br.com.borges.leonorico.desafiotraduzcep.operadores;
+
+import br.com.borges.leonorico.desafiotraduzcep.Endereco;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.List;
+
+public class Serializador {
+
+    Gson gson;
+
+    public Serializador() {
+        gson = new GsonBuilder().setPrettyPrinting().create();
+    }
+
+    public void converteListaEnderecosParaJson(List<Endereco> lista) {
+        try {
+            FileWriter writer = new FileWriter("enderecos.json");
+            writer.write(gson.toJson(lista));
+            writer.close();
+        } catch(IOException e) {
+            throw new RuntimeException("Não foi possível gravar os dados em arquivo.");
+        }
+    }
 }
